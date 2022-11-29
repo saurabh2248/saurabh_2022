99599f10d918a1d21b8d2ddf80b9a7a5d3c48772
unknown <cogjava4586@iiht.tech>
2022-11-29 15:02:02 +0530

    Predicate functional interface

--- Java_basics/Java8-Basics/src/com/basic/functionalInterface/PredicateDemo.java
+++ Java_basics/Java8-Basics/src/com/basic/functionalInterface/PredicateDemo.java
@@ -1,17 +1,17 @@
 package com.basic.functionalInterface;
 
+import java.util.function.Predicate;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
-
-public class PredicateDemo {
-	public static void main(String []args) {
-		//Consumer<Integer> consumer= (t) -> System.out.println("Printing :"+ t);
-	    // consumer.accept(5000);
-		    
-		    List<Integer> numberList= Arrays.asList(1,2,3,4,5);
-		    numberList.stream().forEach((t) -> System.out.println("Printing :"+ t));
-		    
+  public class PredicateDemo implements Predicate<Integer> {
+	@Override
+	public boolean test(Integer t) {
+		if(t%2==0) return true;
+		return false;
+	}
+	
+    public static void main(String []args) {
+    	Predicate<Integer> predicate = new PredicateDemo();
+    	System.out.println(predicate.test(11));
+    	System.out.println(predicate.test(10));
 	}
 }
