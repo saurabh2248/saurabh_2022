7be8381128d4f6662b90b3371d5380d1f1f0ca2a
unknown <cogjava4586@iiht.tech>
2022-12-06 07:12:44 +0530

    Add all spring-1 project

---
+++ Java_basics/Exams/src/com/competitive/exam/ExamAttributes.java
@@ -0,0 +1,63 @@
+package com.competitive.exam;
+
+
+
+public class ExamAttributes {
+	private Integer course_id;
+	private String  course_name;
+    private String  course_teacher;
+    private Integer course_duration;
+    private Integer total_fees;
+    
+    
+	public ExamAttributes(Integer course_id, String course_name, String course_teacher, Integer course_duration,
+			Integer total_fees) {
+		super();
+		this.course_id = course_id;
+		this.course_name = course_name;
+		this.course_teacher = course_teacher;
+		this.course_duration = course_duration;
+		this.total_fees = total_fees;
+	}
+	
+	
+	public Integer getCourse_id() {
+		return course_id;
+	}
+	public void setCourse_id(Integer course_id) {
+		this.course_id = course_id;
+	}
+	public String getCourse_name() {
+		return course_name;
+	}
+	public void setCourse_name(String course_name) {
+		this.course_name = course_name;
+	}
+	public String getCourse_teacher() {
+		return course_teacher;
+	}
+	public void setCourse_teacher(String course_teacher) {
+		this.course_teacher = course_teacher;
+	}
+	public Integer getCourse_duration() {
+		return course_duration;
+	}
+	public void setCourse_duration(Integer course_duration) {
+		this.course_duration = course_duration;
+	}
+	public Integer getTotal_fees() {
+		return total_fees;
+	}
+	public void setTotal_fees(Integer total_fees) {
+		this.total_fees = total_fees;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return course_id + "\t" + course_name + "\t" + course_teacher + "\t" + course_duration + "\t" + total_fees + "\n";
+	}
+	
+    
+	
+}
---
+++ Sprint-1/RegistraionForm.js
@@ -0,0 +1,19 @@
+
+
+function consoleData(){
+    console.log("Full Name:-"+document.getElementById("name").value);
+    console.log("Date of Birth:-"+document.getElementById("DOB").value);
+    console.log("Email:-"+document.getElementById("email").value);
+    console.log("Mobile Number:-"+document.getElementById("mobile").value);
+    console.log("Gender:-"+document.getElementById("gender").value);
+    console.log("Occupation:-"+document.getElementById("Occupation").value);
+    console.log("ID Type:-"+document.getElementById("ID Type").value);
+    console.log("ID Number:-"+document.getElementById("ID Number").value);
+    console.log("Issue Authority:-"+document.getElementById("Issue Authority").value);
+    console.log("Issue Date:-"+document.getElementById("Issue Date").value);
+    console.log("Issue State:-"+document.getElementById("Issue State").value);
+    console.log("Expiry Date:-"+document.getElementById("Expiry Date").value);
+    
+}
+
+
