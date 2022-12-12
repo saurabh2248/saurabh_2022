3318b8a94e23813057b4f3ba2c1a1f4382bfa551
unknown <cogjava4586@iiht.tech>
2022-12-12 01:56:57 -0800

    Completed JWT Token

--- Server_side/JWT-Token/src/main/java/com/token/config/SecurityConfiguration.java
+++ Server_side/JWT-Token/src/main/java/com/token/config/SecurityConfiguration.java
@@ -8,7 +8,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.token.filter.JwtFilter;
 import com.token.service.UserService;
 
 @SuppressWarnings("deprecation")
@@ -18,6 +21,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private JwtFilter jwtFilter;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -30,15 +36,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		return super.authenticationManager();
 	}
 
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf()
-		.disable()
-		.authorizeRequests()
-		.antMatchers("/auth")
-		.permitAll()
-		.anyRequest()
-		.authenticated();
+			http
+			.csrf()
+		    .disable()
+		    .authorizeRequests()
+		    .antMatchers("/auth")
+		    .permitAll()
+		    .anyRequest()
+		    .authenticated()
+		    .and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+			http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
